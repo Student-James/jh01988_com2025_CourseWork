d3750bfbf45e06c0949ebc4b9468af5a7e781788
James Head <james@email>
2020-12-05 02:49:36 -0800

    Working blank home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #Adding the route to the home page.
+  root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
