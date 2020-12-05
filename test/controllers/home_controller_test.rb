1e6723bc4e632cc2b6f67b65c5b2413ca290b4e9
James Head <james@email>
2020-12-05 03:03:31 -0800

    Working home page and tests working

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
+
+    assert_select 'title','My Music Review'
+    assert_select 'h1','My Music Review'
+    assert_select 'p','Welcome to My Music Review','This website is deadicated to allowing users to upload album and tracking listing. They are also able to post their own reviews of each track.'
+    #Testing that the home page is retrieved through the controller.
     assert_response :success
   end
 
