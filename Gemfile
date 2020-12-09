f4f6aaba57ace7a157832500cdc725dddea7f683
James Head <james@email>
2020-12-09 03:12:14 -0800

    Added jquery but not working

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'bootstrap-sass','~>3.3.6'
 gem 'modernizr-rails'
 gem 'rails-controller-testing'
 gem 'devise'
+gem 'jquery-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,63 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require bootstrap-sprockets
-//= require rails-ujs
+//= require jquery3
+//=require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
+function ValidateAlbum(){
+  $('#album').validate({
+    rules: {
+      'album[artist]': {required: true},
+      'album[title]': {required: true},
+    },
+    messages: {
+      'album[artist]': {required: 'Every artist must have a name'},
+      'album[title]': {required: 'Every album must have a title'},
+    }
+  });
+}
+
+function ValidateTrack(){
+  $('#track').validate({
+    rules: {
+      'track[title]': {required: true},
+    },
+    messages: {
+      'track[title]': {required: 'Every song must have a title'},
+    }
+  });
+}
+
+function ValidateReview(){
+  $('#review').validate({
+    rules: {
+      'review[title]': {required: true},
+      'review[reviewbody]': {required: true},
+    },
+    messages: {
+      'review[title]': {required: 'Every review must have a title'},
+      'review[reviewbody]': {required: 'You need to write a review'},
+
+    }
+  });
+}
+
+$(document).on(function(){
+  if(document.getElementById('album')){
+    ValidateAlbum();
+  }
+  if(document.getElementById('track')){
+    ValidateAlbum();
+  }
+  if(document.getElementById('review')){
+    ValidateReview();
+  }
+});
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
